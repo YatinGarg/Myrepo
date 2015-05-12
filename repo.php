9287f95dd0e56ff8e24569f4ba0ad3937c2d153b
YatinGarg <yatingarg12@gmail.com>
2015-05-12 15:34:45 +0530

    my first repo

---
+++ repo.php
@@ -0,0 +1,18 @@
+<html>
+<form action='scnd.php' method='GET'>
+<input type='password' name='myname'><br>
+<input type='submit' value='click here'>
+</form>
+<br>
+<input type='text' name='cude' value='slurp'>
+<html>
+<?php
+echo $_GET['myname'];
+$goto = "https://www.google.co.in/";
+
+if (isset($goto))
+{
+	echo $goto;
+	header("Location: $goto");
+}
+?>
